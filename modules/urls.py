070fc5a6d47446fac7ac9e4fea5d6d248a69d02f
Shirleen Onim <shirleenawinja06@gmail.com>
2024-06-18 12:20:44 +0300

    added job details

--- modules/urls.py
+++ modules/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('modules/education', views.education, name='education'),
+    path('education', views.education, name='education'),
     path('modules/documents', views.add_documents, name='documents'),
     path('modules/work', views.work_experience, name="work"),
     path('modules/profile', views.profile_user, name="profile-user"),
